c9bba579798a3491e4acf87ba38c5e1db7f51e50
leo <vb.zhai@gmail.com>
2019-12-22 19:53:47 +0800

    add tranditional chinese

--- convert.go
+++ convert.go
@@ -9,6 +9,7 @@ import (
 	"golang.org/x/text/encoding/japanese"
 	"golang.org/x/text/encoding/korean"
 	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/traditionalchinese"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/encoding/unicode/utf32"
 	"golang.org/x/text/transform"
@@ -18,14 +19,15 @@ func transformEncoding(rawReader io.Reader, trans transform.Transformer) io.Read
 	return transform.NewReader(rawReader, trans)
 }
 
+//NewTransformer 根据输入流使用 icu 相同的算法，探测流的编码，将流转换为 utf8 编码
 func NewTransformer(r io.ReadSeeker) (io.Reader, error) {
-	var detect = make([]byte, 128)
+	var detect = make([]byte, 256)
 	_, err := r.Read(detect)
 	if err != nil {
 		return nil, err
 	}
 
-	if _, err := r.Seek(0, io.SeekStart);err != nil {
+	if _, err := r.Seek(0, io.SeekStart); err != nil {
 		return nil, err
 	}
 	charDetector := chardet.NewTextDetector()
@@ -38,28 +40,30 @@ func NewTransformer(r io.ReadSeeker) (io.Reader, error) {
 		return transformEncoding(r, simplifiedchinese.GB18030.NewDecoder()), nil
 	case "BIG":
 		return transformEncoding(r, simplifiedchinese.GBK.NewDecoder()), nil
+	case "BIG5":
+		return transformEncoding(r, traditionalchinese.Big5.NewDecoder()), nil
 	case "ISO-2022-CN":
-		return nil, errors.Errorf("%s not supported",detectResult.Charset)
+		return nil, errors.Errorf("%s not supported", detectResult.Charset)
 	case "ISO-2022-JP":
-		return transformEncoding(r,japanese.ISO2022JP.NewDecoder()), nil
+		return transformEncoding(r, japanese.ISO2022JP.NewDecoder()), nil
 	case "ISO-2022-KR":
-		return nil, errors.Errorf("%s not supported",detectResult.Charset)
+		return nil, errors.Errorf("%s not supported", detectResult.Charset)
 	case "EUC-KR":
 		return transformEncoding(r, korean.EUCKR.NewDecoder()), nil
 	case "EUC-JP":
-		return transformEncoding(r,japanese.EUCJP.NewDecoder()), nil
+		return transformEncoding(r, japanese.EUCJP.NewDecoder()), nil
 	case "Shift_JIS":
-		return transformEncoding(r,japanese.ShiftJIS.NewDecoder()), nil
+		return transformEncoding(r, japanese.ShiftJIS.NewDecoder()), nil
 	case "ISO-8859-1":
-		return transformEncoding(r,charmap.ISO8859_1.NewDecoder()), nil
+		return transformEncoding(r, charmap.ISO8859_1.NewDecoder()), nil
 	case "UTF-16LE":
-		return transformEncoding(r,unicode.UTF16(unicode.LittleEndian,unicode.IgnoreBOM).NewDecoder()), nil
+		return transformEncoding(r, unicode.UTF16(unicode.LittleEndian, unicode.IgnoreBOM).NewDecoder()), nil
 	case "UTF-16BE":
-		return transformEncoding(r,unicode.UTF16(unicode.BigEndian,unicode.IgnoreBOM).NewDecoder()), nil
+		return transformEncoding(r, unicode.UTF16(unicode.BigEndian, unicode.IgnoreBOM).NewDecoder()), nil
 	case "UTF-32LE":
-		return transformEncoding(r,utf32.UTF32(utf32.LittleEndian,utf32.IgnoreBOM).NewDecoder()), nil
+		return transformEncoding(r, utf32.UTF32(utf32.LittleEndian, utf32.IgnoreBOM).NewDecoder()), nil
 	case "UTF-32BE":
-		return transformEncoding(r,utf32.UTF32(utf32.BigEndian,utf32.IgnoreBOM).NewDecoder()), nil
+		return transformEncoding(r, utf32.UTF32(utf32.BigEndian, utf32.IgnoreBOM).NewDecoder()), nil
 
 	default:
 		return r, nil
